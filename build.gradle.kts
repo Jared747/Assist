2719770d7d318c4406dbad6c8a72d8e74b4749db
Jared McCallum <jaridmccallum@gmail.com>
2025-09-06 11:51:03 -0700

    Add root build.gradle.kts file

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.3.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+        classpath("io.ktor:ktor-gradle-plugin:3.0.0")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
